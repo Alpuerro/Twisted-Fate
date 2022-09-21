53514d4ad13d81fc09d99e30da6f77f1667f9a4a
Alpuerro <43877321+Alpuerro@users.noreply.github.com>
2022-09-21 21:35:05 +0200

    Enemy random actions and state added

--- Twisted Fate/Assets/Scripts/Enemy/Enemy.cs
+++ Twisted Fate/Assets/Scripts/Enemy/Enemy.cs
@@ -3,33 +3,62 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Enemy", menuName = "ScriptableObjects/Enemy")]
 public class Enemy : ScriptableObject
 {
+    [Space(10)]
+    public EnemyType enemyType;
+    [Space(10)]
+    public int level = 1;
+    public int health;
+    public int attack;
+    public int defense;
+    public int shield;
+    public bool isStunned;
+    private EnemyAction enemyAction;
+
+    [Space(10)]
     [SerializeField]
     [Range(0, 1)]
     [Tooltip("Percentage of the level that is multiplied to the base damage stat.")]
     /// <summary> Percentage of the level that is multiplied to the base damage stat. </summary>
     private float _damagePerLevelMultiplier;
+    [SerializeField]
+    [Range(0, 1)]
+    [Tooltip("Percentage of the level that is multiplied to the base defense stat.")]
+    /// <summary> Percentage of the level that is multiplied to the base defense stat. </summary>
+    private float _defensePerLevelMultiplier;
 
-    public EnemyType enemyType;
-    public int level = 1;
-    public int health;
-    public int defense;
-    public int baseDamage;
+    [Space(10)]
+    public Texture2D enemySprite;
+    public Texture2DArray actionIcons;
+    public Animator animator;
 
-    public int AttackDamage
+    public int AttackDamage { get { return (int)(attack * ((level * _damagePerLevelMultiplier) + 1)); } }
+    public int ShieldAmmount { get { return (int)(defense * ((level * _defensePerLevelMultiplier) + 1)); } }
+
+    public ref EnemyAction GetEnemyAction()
     {
-        get { return (int)(baseDamage * ((level * _damagePerLevelMultiplier) + 1)); }
+        enemyAction.enemyActionsType = ((Random.value < 0.5) ? EnemyActionsType.Attack : EnemyActionsType.Defend);
+        switch (enemyAction.enemyActionsType)
+        {
+            case EnemyActionsType.Attack:
+                enemyAction.ammout = AttackDamage;
+                break;
+            case EnemyActionsType.Defend:
+                enemyAction.ammout = ShieldAmmount;
+                break;
+            default:
+                enemyAction.ammout = 0;
+                break;
+        }
+
+        return ref enemyAction;
     }
 }
 
-public enum EnemyType
+public struct EnemyAction
 {
-    Small,
-    Medium,
-    Large
+    public int ammout;
+    public EnemyActionsType enemyActionsType;
 }
 
-public enum EnemyActions
-{
-    Attack,
-    Defend
-}
+public enum EnemyType { Small, Medium, Large }
+public enum EnemyActionsType { Attack, Defend }
